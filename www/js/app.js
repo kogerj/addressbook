ff70ad836745c6a60a810c9ac7f9af0238c3c54b
Jaroslava Koger <kogerj@tlu.ee>
2019-04-24 12:49:48 -0700

    Added User Viev

--- www/js/app.js
+++ www/js/app.js
@@ -6,13 +6,25 @@
     EmployeeListView.prototype.template = 
             Handlebars.compile($("#employee-list-tpl").html());
 
+
+    EmployeeView.prototype.template = Handlebars.compile($("#employee-tpl").html());  
+
     var employeeListTpl = Handlebars.compile($("#employee-list-tpl").html());
 
     var service = new EmployeeService();
-    
+
     service.initialize().done(function () {
         $('body').html(new HomeView(service).render().$el);
     });
+
+    router.addRoute('employees/:id', function(id) {
+        service.findById(parseInt(id)).done(function(employee) {
+            $('body').html(new EmployeeView(employee).render().$el);
+        });
+    });
+  
+    router.start();
+
 
     /* --------------------------------- Event Registration -------------------------------- */
 
