868bbbf3c4112a4acc065a7fe78401e9e7ab635f
Jaroslava Koger <kogerj@tlu.ee>
2019-04-24 13:30:34 -0700

    added add location, add to contacts and change picture options

--- www/js/app.js
+++ www/js/app.js
@@ -9,21 +9,20 @@
 
     EmployeeView.prototype.template = Handlebars.compile($("#employee-tpl").html());  
 
-    var employeeListTpl = Handlebars.compile($("#employee-list-tpl").html());
-
     var service = new EmployeeService();
-
     service.initialize().done(function () {
-        $('body').html(new HomeView(service).render().$el);
-    });
-
-    router.addRoute('employees/:id', function(id) {
-        service.findById(parseInt(id)).done(function(employee) {
-            $('body').html(new EmployeeView(employee).render().$el);
+        router.addRoute('', function() {
+            $('body').html(new HomeView(service).render().$el);
         });
-    });
-  
-    router.start();
+      
+        router.addRoute('employees/:id', function(id) {
+            service.findById(parseInt(id)).done(function(employee) {
+                $('body').html(new EmployeeView(employee).render().$el);
+            });
+        });
+      
+        router.start();
+      });
 
 
     /* --------------------------------- Event Registration -------------------------------- */
